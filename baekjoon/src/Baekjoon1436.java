9b0da36d8402bb57c941c48bb3921f095b3d59c3
ahyun98 <dldkgus98@naver.com>
2022-03-13 00:47:49 +0900

    2022.03.11

--- baekjoon/src/Baekjoon1436.java
+++ baekjoon/src/Baekjoon1436.java
@@ -8,19 +8,25 @@ public class Baekjoon1436 {
 		int[] arr = new int[10000];
 		arr[0] = 666;
 		int id = 1;
-		int i = 1;
 		int t = 0;
+		int k = 0;
+		for(int j=1; j<6; j++) {
+			arr[id] = j*1000+num;
+			id++;
+			k++;
+		}
+		
 		while (id < 9000) {
-			while (6 * t < i && i < t + 6) {
-				arr[id] = i * 1000 + num;
-				id++;
-				i++;
+			if(id<9000) {
+				for (int j = 0; j <= 9; j++) {
+					arr[id] = t * 100 + num * 10 + j;
+					id++;
+				}			
 			}
-			for (int j = 0; j <= 9; j++) {
-				arr[id] = t * 100 + num * 10 + j;
-				id++;
+			for (int j = 10*t+7; j <(t+1)*10+6; j++) {
+				arr[id++] = j*1000+num; 
 			}
-			t *= 10;
+			t++;
 		}
 
 		for (int j = 0; j < arr.length; j++) {
