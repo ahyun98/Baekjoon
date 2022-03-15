2037e988a4deeab4ba955c614d8c049422a50802
ahyun98 <dldkgus98@naver.com>
2022-03-15 23:24:02 +0900

    s

--- baekjoon/src/Baekjoon11000.java
+++ baekjoon/src/Baekjoon11000.java
@@ -1,4 +1,10 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Baekjoon11000 {
-
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+	}
 }
